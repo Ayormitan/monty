c95274b857ea719c6496352577cf2ad632757e59
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-13 14:58:58 -0800

    String stacking function

---
+++ str_stack.c
@@ -0,0 +1,85 @@
+#include "monty.h"
+/**
+ *
+ *
+ *
+ */
+void string_prints(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	int ascii;
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	temp = *stack;
+	while (temp != NULL)
+	{
+		ascii = temp->n;
+		if (ascii <= 0 || ascii > 127)
+			break;
+		printf("%c", ascii);
+		temp = temp->next;
+	}
+	printf("\n");
+}
+/**
+ *
+ *
+ *
+ */
+void char_print(stack_t **stack, unsigned int line_num)
+{
+	int ascii;
+
+	if (stack == NULL || *stack == NULL)
+		string_err(11, line_num);
+	ascii = (*stack)->n;
+	if (ascii < 0 || ascii > 127)
+		string_err(10, line_num);
+	printf("%c\n", ascii);
+}
+/**
+ *
+ *
+ */
+void rot1(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = (*stack)->next;
+	(*stack)->prev->next = NULL;
+	(*stack)->prev = NULL;
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		return;
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = *stack;
+	temp->prev->next = NULL;
+	temp->prev = NULL;
+	(*stack)->prev = temp;
+	(*stack) = temp;
+}
