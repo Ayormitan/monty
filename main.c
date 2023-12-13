03f90ecddac2c4b11c0868eab6928f665410bd05
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-13 14:56:12 -0800

    Main test function

---
+++ main.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+stack_t *head = NUUL;
+/**
+ *
+ *
+ *
+ */
+int main(int argc, chr *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILAYRE);
+	}
+	open_file(argv[1]);
+	free_nodes();
+	return (0);
+}
+/**
+ *
+ *
+ *
+ */
+stack_t *make_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		err(4);
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+/**
+ *
+ *
+ *
+ */
+void add2queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *temp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = *new_node;
+	(*new_node)->prev = temp;
+}
+/**
+ *
+ *
+ *
+ */
+void freenodes(void)
+{
+	stack_t *temp;
+
+	if (head == NULL)
+		return;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
