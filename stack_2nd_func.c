d7f4f84038c28f74b640edbf634a106d62b06156
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-13 14:58:21 -0800

    Another file handling some func

---
+++ stack_2nd_func.c
@@ -0,0 +1,86 @@
+#include "monty.h"
+/**
+ *
+ *
+ *
+ */
+void nodeswap(stack_t **stack, unsigned int line_num)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_num, "swap");
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		tmp->next->prev = *stack;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	temp->prev = NULL;
+	*stack = temp;
+}
+/**
+ *
+ *
+ *
+ */
+void nop(stack_t **stack, unsigned  int line_num)
+{
+	(void)stack;
+	(void)line_num;
+}
+/**
+ *
+ *
+ */
+void node_div(stack_t **stack, unsigned int line_num)
+{
+	int summat;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_num, "div");
+	if ((*stack)->n == 0)
+		more_err(9, line_num);
+	(*stack) = (*stack)->next;
+	summat = (*stack)-> / (*stack)->prev->n;
+	(*stack)->n = summat;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void node_add(stack_t **stack, unsigned int line_num)
+{
+	int summat;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_num, "add");
+	(*stack) = (*stack)->next;
+	summat = (*stack)->n + (*stack)->prev-n;
+	(*stack)->n = summat;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+/**
+ *
+ *
+ *
+ *
+ */
+void node_sub(stack_t **stack, unsigned int line_num)
+{
+	int summat;
+
+	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
+		more_err(8, line_num, "sub");
+	(*stack) = (*stack)->next;
+	summat = (*stack)->n - (*stack)->prev->n;
+	(*stack)->n = summat;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
