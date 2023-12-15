0c3add37af412df970f0e5db4cf8c0e0ffaac303
Betvic23 <brightndubisi23@gmail.com>
2023-12-15 14:32:36 -0800

    my stack func1

--- my_stack_func1.c
+++ my_stack_func1.c
@@ -1,5 +1,8 @@
 #include "monty.h"
 /**
+ * add_to_stack - adds node to stack
+ * @new_node - pointer to new node
+ * @en: int represnting line num
  */
 void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int en)
 {
@@ -20,6 +23,11 @@ void add_to_stack(stack_t **new_node, __attribute__((unused))unsigned int en)
 	temp->prev = head;
 }
 
+/**
+ * print_my_stack - adds node to stack
+ * @line_num: line number
+ * @stack: pointer pointer that points to top node
+ */
 void print_my_stack(stack_t **stack, unsigned int line_num)
 {
 	stack_t *temp;
@@ -38,4 +46,33 @@ void print_my_stack(stack_t **stack, unsigned int line_num)
 	}
 }
 
-void pop_on_top(
+/**
+ * pop_on_top - adds node to the stack
+ * @line_num: int represneting ine number
+ * @stack: pointer pointer that points to top node
+ */
+void pop_on_top(stack_t **stack, unsigned int line_num)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+		more_err(7, line_num);
+
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack != NULL)
+		(*stack)->prev = next;
+	free(temp);
+}
+
+/**
+ * top_print - prints top node of stack
+ * @stack: pointer pointer that points to top node
+ * @line_num: int representing line numbe
+ */
+void top_print(stack_t **stack, unsigned int line_num)
+{
+	if (stack == NULL || *stack == NULL)
+		more_err(6, line_num);
+	printf("%d\n", (*stack)->n);
+}
