6a4fc1566dcbb3463473280a6042bef2cb9ab116
Ayormitan <kolawoleabaz97@gmail.com>
2023-12-17 13:44:07 -0800

    Header file

--- monty.h
+++ monty.h
@@ -1,6 +1,14 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdarg.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
